fe39e82daba86c88f8b9985d148407549cef5e19
Sak <jacubsak@gmail.com>
2023-12-21 16:08:30 +0100

    footer update spelling error

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-100 bg-pink-800 text-white mb-auto">
-      <h4 class="text-center py-5">Angry Indian Woman Sharmila S © 32.09.2043</h4>
+      <h4 class="text-center py-5"> Garmażeria Industries © 2023</h4>
   </div>
 </template>
